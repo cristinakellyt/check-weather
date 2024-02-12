4b82a1390ed4285b8897ad57d5a11b0b278d1790
cristinakellyt <cristinakellyt@gmail.com>
2024-02-12 13:38:40 +0100

    feat: create a search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,61 @@
 <template>
-  <main>
-    <h1>Main</h1>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        @input="getSearchResults"
+        v-model="searchQuery"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your search, try a different term.
+        </p>
+
+        <template v-else>
+          <li v-for="result in mapboxSearchResults" :key="result.id" class="py-2 cursor-pointer">
+            {{ result.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
+const mapboxAPIKey =
+  'pk.eyJ1IjoiY3Jpc3RpbmFrIiwiYSI6ImNsc2l3NnZzazE5ZGEya3F0bmZpaG5jaGIifQ.XHAmegdsFb_CqekE5Etg2w'
+
+const mapboxSearchResults = ref()
+const searchQuery = ref('')
+const queryTimeout = ref()
+const searchError = ref(false)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value)
+
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        )
+        mapboxSearchResults.value = result.data.features
+      } catch {
+        searchError.value = true
+      }
+      return
+    }
+    mapboxSearchResults.value = null
+  }, 300)
+}
+</script>
